fe829482d4d40537ed87d8cbf1765e01a464f72c
zheleu-yergazy <ergazyjeleu@gmail.com>
2025-11-27 23:12:22 +0500

    Update connect.py

--- LAB10/connect.py
+++ LAB10/connect.py
@@ -2,9 +2,9 @@ import psycopg2
 from config import load_config
 
 def connect(config):
-    """ Connect to the PostgreSQL database server """
+   
     try:
-        # connecting to the PostgreSQL server
+       
         with psycopg2.connect(**config) as conn:
             print('Connected to the PostgreSQL server.')
             return conn
